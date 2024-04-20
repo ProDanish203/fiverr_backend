6d9be797cfad90df2032c5148e6a8626b42971d1
Danish <danishsidd524@gmail.com>
2024-04-20 20:46:58 +0500

    add paginated users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { User } from "../models/user.model.js";
 import { sendVerificationMail } from "../utils/mailer.js";
 import { uploadFile, deleteFile } from "../utils/fileUpload.js";
+import { getPaginatedData, getPaginatedUsers } from "../utils/helpers.js";
 
 export const getCurrentUser = async (req, res, next) => {
     try {
@@ -74,8 +75,31 @@ export const verifyEmail = async (req, res, next) => {
     }
 };
 
+// Admin Panel API
 export const getAllUsers = async (req, res, next) => {
     try {
+        const page = +(req.query.page || 1);
+        const limit = +(req.query.limit || 10);
+        const search = req.query.search || "";
+        const filter = req.query.filter || "";
+        let sortDirection = 1;
+
+        if (filter.toLowerCase() === "ztoa") {
+            sortDirection = -1;
+        }
+
+        const users = await getPaginatedUsers({
+            query: { username: { $regex: `^${search}`, $options: "i" } },
+            page,
+            limit,
+            sort: { username: sortDirection },
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: "All users",
+            data: users,
+        });
     } catch (error) {
         console.log(error);
         next(error);
---
+++ src/utils/helpers.js
@@ -0,0 +1,63 @@
+import { User } from "../models/user.model.js";
+
+export const getPaginatedData = async ({
+    model,
+    page = 1,
+    limit = 10,
+    query = {},
+    populate = "",
+    select = "-password -refreshToken -forgotPasswordToken -forgotPasswordTokenExpiry -verifyToken -verifyTokenExpiry",
+    sort = { createdAt: -1 },
+}) => {
+    const options = {
+        select,
+        sort,
+        page,
+        limit,
+        populate,
+        lean: true,
+        customLabels: {
+            totalDocs: "totalItems",
+            docs: "data",
+            limit: "perPage",
+            page: "currentPage",
+            meta: "pagination",
+        },
+    };
+
+    const { data, pagination } = await model.paginate(query, options);
+    delete pagination?.pagingCounter;
+
+    // const skip = (page - 1) * limit;
+    // const data = await model
+    //     .find(query, select)
+    //     .populate(populate)
+    //     .sort(sort)
+    //     .skip(skip)
+    //     .limit(limit)
+    //     .lean();
+
+    // const totalItems = await model.countDocuments(query);
+    // const totalPages = Math.ceil(totalItems / limit);
+
+    // const pagination = {
+    //     totalItems,
+    //     totalPages,
+    //     currentPage: page,
+    //     perPage: limit,
+    // };
+
+    return { data, pagination };
+};
+
+export const getPaginatedUsers = async ({ query, page, limit, sort }) => {
+    const { data, pagination } = await getPaginatedData({
+        model: User,
+        query: { ...query, role: "user" },
+        page,
+        limit,
+        sort,
+    });
+
+    return { data, pagination };
+};
